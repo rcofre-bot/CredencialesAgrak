5b05b03fbc291e219273f712523179769c07b10a
Rodrigo Cofre <rcofre@mayecura.cl>
2026-07-02 13:32:45 -0400

    actualizacion impresion 3

--- src/components/TarjasManager.jsx
+++ src/components/TarjasManager.jsx
@@ -259,7 +259,7 @@ export default function TarjasManager({ camposList, empresasMaestras }) {
     return html;
   };
 
-  // 🔥 CALIBRACIÓN FINA (CODIGO CSG CENTRADO) 🔥
+  // 🔥 CALIBRACIÓN FINA (CODIGO CSG DENTRO DEL CUADRO BLANCO) 🔥
   const getPlantillaTorretagleTarja = (tarjasAImprimir) => {
     let html = `<!DOCTYPE html>
     <html>
@@ -293,8 +293,8 @@ export default function TarjasManager({ camposList, empresasMaestras }) {
         
         .box-cantidad  { top: 22mm; left: 32mm; width: 42mm; height: 12mm; font-size: 11pt; }
         
-        /* CORRECCIÓN: El CSG se movió al punto medio perfecto (35mm) */
-        .box-csg       { top: 35mm; left: 32mm; width: 42mm; height: 12mm; font-size: 11pt; }
+        /* CORRECCIÓN: El CSG se movió de 35mm a 37mm para que entre y se centre en el cuadro blanco. */
+        .box-csg       { top: 37mm; left: 32mm; width: 42mm; height: 12mm; font-size: 11pt; }
         
         .box-productor { top: 58mm; left: 6mm; width: 68mm; height: 14mm; font-size: 11pt; }
         .box-especie   { top: 79mm; left: 6mm; width: 68mm; height: 14mm; font-size: 11pt; }
